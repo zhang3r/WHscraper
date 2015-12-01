3cd41c68f24c1ebee20da3e1db594c6edb8f9b75
Ruiqi Zhang <rzhang@roundarchisobar.com>
2015-12-01 14:30:09 -0500

    updated to python 3

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 """scraper for hacker news who's hiring post"""
-from urllib2 import urlopen
+from urllib.request import urlopen
 import unicodedata
 import codecs
 
@@ -16,25 +16,25 @@ def scraper(url):
 		for line in website:
 			line_lowercase = line.lower()
 
-			if (' sf' in line_lowercase or 'sf ' in line_lowercase or 'san francisco' in line_lowercase or 'mountain view' in line_lowercase or 'sunnyvale' in line_lowercase  or 'CA ' in line_lowercase or ' CA' in line_lowercase) and ('python' in line_lowercase or 'java ' in line_lowercase or 'java,' in line_lowercase or 'java/' in line_lowercase or 'Django' in line_lowercase):
+			if (' sf' in line_lowercase or 'sf ' in line_lowercase or 'san francisco' in line_lowercase or 'palo alto' in line_lowercase or 'sunnyvale' in line_lowercase  or 'CA ' in line_lowercase or 'CA' in line) and ('python' in line_lowercase or 'java ' in line_lowercase or 'java,' in line_lowercase or 'java/' in line_lowercase or 'Django' in line_lowercase):
 				if i:
 					f.write("<ul style='background-color:lightcoral'>")
 				else:
 					f.write("<ul style='background-color: lightsteelblue'>")
-				f.write(unicodedata.normalize('NFKD', line).encode('ascii','ignore'))
+				f.write(line)
 				f.write("</ul>")
 				f.write("<hr>")
 				i+=1
 				i&=1
 	f.close()
-	print "done :D"
+	print("done :D")
 
 
 
 
 def main():
 	# flags = raw_input()
-	url = "https://news.ycombinator.com/item?id=10492086"
+	url = "https://news.ycombinator.com/item?id=10655740"
 	scraper(url)
 	
 
